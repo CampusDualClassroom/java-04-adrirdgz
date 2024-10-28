79d27ce0c983dd1752d13bba70a6166b91decd56
Adrian Rodriguez Diez <adrian.r.d12389@gmail.com>
2024-10-28 15:24:42 +0100

    Ejercicio 4 corregido

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -2,7 +2,6 @@ package com.campusdual.classroom;
 
 public class Exercise04 {
     public static void main(String[] args) {
-
         System.out.println("Ejemplos de cálculos SIN estructuras de control:");
         System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
         System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
@@ -14,11 +13,10 @@ public class Exercise04 {
         System.out.println("Suma con sumNum(5): " + sumNum(5)); // 15
         System.out.println("Suma de los primeros 20 números pares con sumEvenNum(20): " + sumEvenNum(20)); // 420
         System.out.println("Factorial con factorial(7): " + factorial(7)); // 5040
-
+        System.out.println("Factorial recursivo con recursiveFactorial(7): " + recursiveFactorial(7)); // 5040
     }
 
     public static int sumNum(int num) {
-
         int sum = 0;
         for (int i = 1; i <= num; i++) {
             sum += i;
@@ -42,5 +40,13 @@ public class Exercise04 {
             result *= i;
         }
         return result;
+    }
+
+    public static int recursiveFactorial(int num) {
+        if (num <= 1) { // Caso base
+            return 1;
+        } else {
+            return num * recursiveFactorial(num - 1);
+        }
     }
 }
